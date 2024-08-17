2542fff884d99d3fb1ed6b13f9f00303626dd766
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-08-17 22:56:34 +0530

    Attach GitHub profile link to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {IoHeart} from "@react-icons/all-files/io5/IoHeart";
+import { IoHeart } from "@react-icons/all-files/io5/IoHeart";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
-
   return (
     <footer className="mt-auto">
       <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
@@ -12,11 +12,13 @@ const Footer: React.FC = () => {
             className="text-lg dark:text-gray-100 text-neutral-950"
             aria-label="Heart Icon"
           />
-          &nbsp;by
+          &nbsp;by&nbsp;
         </span>
-        <span className="text-sm text-neutral-950 dark:text-neutral-50 font-extrabold text-center">
-          &nbsp;Nureka Rodrigo
-        </span>
+        <Link to={"https://github.com/nureka-rodrigo"} target="_blank">
+          <span className="text-sm text-neutral-950 dark:text-neutral-50 font-extrabold text-center hover:underline">
+            Nureka Rodrigo
+          </span>
+        </Link>
       </div>
     </footer>
   );
